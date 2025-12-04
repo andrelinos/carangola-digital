15c936ffa605fc3b197e99d410bae46551f3fe9a
Andrelino Silva <andrelinos@gmail.com>
2025-12-04 16:16:45 -0300

    feat: remove sitemap restriction

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,5 @@
 import { type NextRequest, NextResponse } from 'next/server'
 
-import { allowedUserAgents } from '@/configs/allowed-user-agents'
 import { blockedPaths } from '@/configs/blocked-paths'
 
 import { fakeContent } from './assets/data/content-fake'
@@ -10,19 +9,19 @@ export function middleware(request: NextRequest) {
 
   const isProduction = process.env.NODE_ENV === 'production'
 
-  if (isProduction && pathname === '/sitemap.xml') {
-    const userAgent = request.headers.get('user-agent') || ''
-    const isAllowedUserAgent = allowedUserAgents.some(agent =>
-      userAgent.includes(agent)
-    )
+  // if (isProduction && pathname === '/sitemap.xml') {
+  //   const userAgent = request.headers.get('user-agent') || ''
+  //   const isAllowedUserAgent = allowedUserAgents.some(agent =>
+  //     userAgent.includes(agent)
+  //   )
 
-    if (!isAllowedUserAgent) {
-      return new NextResponse(fakeContent, {
-        status: 200,
-        headers: { 'content-type': 'text/html' },
-      })
-    }
-  }
+  //   if (!isAllowedUserAgent) {
+  //     return new NextResponse(fakeContent, {
+  //       status: 200,
+  //       headers: { 'content-type': 'text/html' },
+  //     })
+  //   }
+  // }
 
   if (blockedPaths.some(blocked => pathname.startsWith(blocked))) {
     return new NextResponse(fakeContent, {
